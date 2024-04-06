31273f22facdbb7a59c7b921484e27e8093bea5e
affan <affanabid31@gmail.com>
2024-04-06 17:51:05 +0500

    linkedlist

--- n.py
+++ n.py
@@ -48,10 +48,12 @@
 # name = 'affan'
 # print(len(name))
 
-from collections import Counter
+# from collections import Counter
 
-nums = [-1,2,-1,4,3,2,1]
-c = Counter(nums).most_common(2)
-for a in c:
-    print(a[0])
-# print(c)
+# nums = [-1,2,-1,4,3,2,1]
+# c = Counter(nums).most_common(2)
+# for a in c:
+#     print(a[0])
+# # print(c)
+print(10//10)
+print(20//10)
