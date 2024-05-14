6d5f7f9f02e8c1906fae9adbe16cf393267f0fb7
affan <affan.abid99@gmail.com>
2024-05-15 00:45:29 +0500

    code

---
+++ binarySearchTree/AVL.py
@@ -0,0 +1,112 @@
+from bst_balance import height, isBalanced
+
+class TreeNode:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.left = None
+        self.right = None
+        self.height = 1
+
+class AVLTree:
+    def insert(self, root, key):
+        if not root:
+            return TreeNode(key)
+        elif key < root.value:
+            root.left = self.insert(root.left, key)
+        else:
+            root.right = self.insert(root.right, key)
+
+        root.height = 1 + max(self.getHeight(root.left), self.getHeight(root.right))
+
+        balanceFactor = self.getBalanceFactor(root)
+
+        # case-1: Left-Left
+        if balanceFactor > 1 and key < root.left.value:
+            return self.rightRotation(root)
+        
+        # case-2: Right Right
+        elif balanceFactor < -1 and key > root.right.value:
+            return self.leftRotation(root)
+        
+        # case-3: Left Right
+        elif balanceFactor > 1 and key > root.left.value:
+            root.left = self.leftRotation(root.left)
+            return self.rightRotation(root)
+        
+        elif balanceFactor < -1 and key < root.right.value:
+            root.right = self.rightRotation(root.right)
+            return self.leftRotation(root)
+        
+        return root
+
+    def leftRotation(self, node):
+        child = node.right
+        temp = child.left
+        
+        child.left = node
+        node.right = temp
+
+        node.height = 1 + max(self.getHeight(node.left), self.getHeight(node.right))
+
+        child.height = 1 + max(self.getHeight(child.left), self.getHeight(child.right))
+
+        return child
+
+    def rightRotation(self, node):
+        child = node.left
+        temp = child.right
+
+        child.right = node
+        node.left = temp
+
+        node.height = 1 + max(self.getHeight(node.left), self.getHeight(node.right))
+
+        child.height = 1 + max(self.getHeight(child.left), self.getHeight(child.right))
+
+        return child
+    
+    def getHeight(self, root):
+        if not root:
+            return 0
+        return root.height
+    
+    def getBalanceFactor(self, root):
+        if not root:
+            return 0
+        bf = self.getHeight(root.left) - self.getHeight(root.right)
+        return bf
+
+    def preOrder(self, root): 
+        if not root: 
+            return
+  
+        print("{0} ".format(root.value), end="") 
+        self.preOrder(root.left) 
+        self.preOrder(root.right) 
+
+myTree = AVLTree() 
+root = None
+  
+root = myTree.insert(root, 10) 
+root = myTree.insert(root, 20) 
+root = myTree.insert(root, 30) 
+root = myTree.insert(root, 40) 
+root = myTree.insert(root, 50) 
+root = myTree.insert(root, 25) 
+  
+"""The constructed AVL Tree would be 
+            30 
+           /  \ 
+         20   40 
+        /  \     \ 
+       10  25    50"""
+  
+# Preorder Traversal 
+print("Preorder traversal of the", 
+      "constructed AVL tree is") 
+myTree.preOrder(root) 
+print() 
+if isBalanced(root):
+	print("Tree is balanced")
+else:
+	print("Tree is not balanced")
--- binarySearchTree/adt.py
+++ binarySearchTree/adt.py
@@ -9,35 +9,99 @@ class BinarySearchTree:
         self.root = None
 
     def insert(self, val):
-        self.root = self.insert_recursive(self.root, val)
-
-    def insert_recursive(self, root, key):
-        if root is None:
-            return TreeNode(key)
-
-        if root.val == key:
-            return root
-        elif root.val < key:
-            root.right = self.insert_recursive(root.right, key)
-        elif root.val > key:
-            root.left = self.insert_recursive(root.left, key)
-        return root        
+        if not self.root:
+            self.root = TreeNode(val)
+        else:
+            current = self.root
+            while True:
+                if val < current.val:
+                    if current.left is None:
+                        current.left = TreeNode(val)
+                        break
+                    else:
+                        current = current.left
+                elif val > current.val:
+                    if current.right is None:
+                        current.right = TreeNode(val)
+                        break
+                    else:
+                        current = current.right
+                else:
+                    break
+    
+    def search(self, key):
+        current = self.root
+        while current:
+            if current.val == key:
+                print('Found')
+                return key
+            elif current.val < key:
+                current = current.right
+            elif current.val > key:
+                current = current.left
+        print('Not found')
+        return -1
+    
+    def insert_rec(self, val):
+        self.root = insert_recursive(self.root, val)    
     
     def indorder(self):
-        self.inorder_recursive(self.root)
-    
-    def inorder_recursive(self, root):
-        if root:
-            self.inorder_recursive(root.left)
-            print(root.val, end=' ')
-            self.inorder_recursive(root.right)
+        inorder_recursive(self.root)
+        print()
+
+def inorder_recursive(root):
+    if root:
+        inorder_recursive(root.left)
+        print(root.val, end=' ')
+        inorder_recursive(root.right)
+
+def insert_recursive(root, key):
+    if root is None:
+        return TreeNode(key)
+    else:
+        if key <= root.val:
+            temp = insert_recursive(root.left, key)
+            root.left = temp
+            temp.parent = root
+
+        elif key > root.val:
+            temp = insert_recursive(root.right, key)
+            root.right = temp
+            temp.parent = root
+        return root    
+
+def minNode(node):
+    current = node
+    while current.left:
+        current = current.left
+    return current.val
+
+def maxNode(node):
+    current = node
+    while current.right:
+        current = current.right
+    return current.val
+
+def successor(node):
+    if node.right:
+        return minNode(node.right)
+    parent = node.parent
+    while parent and parent.right == node:
+        node = parent
+        parent = parent.parent
+    return node
 
 if __name__ == '__main__':
     bst = BinarySearchTree()
-    bst.insert(5)
-    bst.insert(1)
-    bst.insert(8)
-    bst.insert(2)
-    bst.insert(9)
-    bst.insert(4)
+    bst.insert_rec(5)
+    bst.insert_rec(1)
+    bst.insert_rec(8)
+    bst.insert_rec(2)
+    bst.insert_rec(9)
+    bst.insert_rec(4)
     bst.indorder()
+    # bst.search(3)
+
+    # print(minNode(bst.root))
+    # print(maxNode(bst.root))
+    print(successor(bst.root))
